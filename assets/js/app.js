d721e7c2d028a5369d9ffe840ac6f65015638940
lucperkins <lucperkins@gmail.com>
2020-03-05 13:03:06 -0800

    Set up Sass pipeline
    Signed-off-by: lucperkins <lucperkins@gmail.com>

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+const navbarBurger = () => {
+  const burger = $(".navbar-burger"),
+    menu = $(".navbar-menu");
+
+  burger.click(() => {
+
+
+    [burger, menu].forEach((el) => el.toggleClass('is-active'));
+  });
+}
+
+$(() => {
+  console.log("Welcome to the CNCF's Hugo + Netlify starter");
+
+  navbarBurger();
+});
